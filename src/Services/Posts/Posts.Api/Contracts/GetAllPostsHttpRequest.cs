fdc734f7aea279a7f0753e6f02b8c4823a4f687c
ismael-soriano <ismaelsorianolozano@gmail.com>
2026-04-24 17:57:00 +0200

    feat: add default values for swagger documentation

--- src/Services/Posts/Posts.Api/Contracts/GetAllPostsHttpRequest.cs
+++ src/Services/Posts/Posts.Api/Contracts/GetAllPostsHttpRequest.cs
@@ -1,4 +1,14 @@
+using System.ComponentModel;
+
 namespace Posts.Api.Contracts;
 
-public sealed record GetAllPostsHttpRequest(int Page = 1, int PageSize = 10, bool IncludeAuthor = false);
+public sealed record GetAllPostsHttpRequest
+{
+    [DefaultValue(1)]
+    public int Page { get; init; } = 1;
+    [DefaultValue(10)]
+    public int PageSize { get; init; } = 10;
+    [DefaultValue(false)]
+    public bool IncludeAuthor { get; init; } = false;
+}
 
